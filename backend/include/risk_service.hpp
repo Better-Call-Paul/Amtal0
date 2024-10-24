c71e4de0bfc538555e70fe4eb73836c529934a5e
Paul Chan <paul.plays.a.pun@gmail.com>
2024-10-24 00:32:56 -0400

    ticker addition

---
+++ backend/include/risk_service.hpp
@@ -0,0 +1,13 @@
+#ifndef RISK_SERVICE_HPP
+#define RISK_SERVICE_HPP
+
+namespace Amtal {
+
+
+
+    
+}
+
+
+
+#endif
---
+++ backend/include/ticker_plant.hpp
@@ -0,0 +1,11 @@
+#ifndef TICKER_PLANT_HPP
+#define TICKER_PLANT_HPP
+
+namespace Amtal {
+
+
+
+}
+
+
+#endif 
